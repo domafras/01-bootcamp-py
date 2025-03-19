2a2f2dc0c330a2ed800bd05b9aec73d94f9d386b
Leonardo Salin <domafras@gmail.com>
2025-03-18 21:05:22 -0300

    adiciona contador de caracteres

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
+# Olá, Mundo!
 print("Hello World!")
+
+# Crie um programa que o usuário digita o seu nome e retorna o número de caracteres
+nome = input("Digite seu nome: ")
+quantidade_caracteres = len(nome)
+print(quantidade_caracteres)
